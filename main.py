bedc8fd0b4a175b604e52591f9f1fbe8c13766e4
Theraggon <theraggon@gmail.com>
2019-06-10 11:00:31 +0200

    Added argument parser for --directory and --interval

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from playsound import playsound
 import os
 import random
 import time
+import argparse
 
 path = '.\Sound'
 interval = 5
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--interval", help="Set interval between sounds. Default=5",
+                    type=int)
+parser.add_argument("-d","--directory", help="Set path to directory with sounds")
+args = parser.parse_args()
+if args.interval:
+    interval = args.interval
+if args.directory:
+    if os.path.isdir(args.directory):
+        path = args.directory
+    else:
+        print("{0} is not a directory. Using default './Sound'".format(args.directory))
 
 files = []
 
