9156316c7eef5657160fd42d2c7206aee3c80007
Theraggon <theraggon@gmail.com>
2019-06-10 10:17:23 +0200

    Select random file from an array, play it and wait in a loop

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from playsound import playsound
 import os
+import random
+import time
 
 path = '.\Sound'
 interval = 5
@@ -9,3 +12,8 @@ for r, d, f in os.walk(path):
     for file in f:
         if '.mp3' or '.wav' or '.aiff' in file:
             files.append(os.path.join(r, file))
+
+while True:
+    randomPath = files[random.randint(0,len(files)-1)]
+    playsound(randomPath, False)
+    time.sleep(interval)
